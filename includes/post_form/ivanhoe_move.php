0f2fbec122f92bdfa4f2c3b388b5c0da2012d385
csbailey5t <csb5t@virginia.edu>
2015-02-24 09:47:51 -0500

    Fixes variable reference, because classes.

--- includes/post_form/ivanhoe_move.php
+++ includes/post_form/ivanhoe_move.php
@@ -37,7 +37,7 @@ class IvanhoeMove extends BasePostForm
         $buffer = sprintf(
             __( 'You are making a move on the game '
                 . '&#8220;<a href="%1$s">%2$s</a>&#8221;', 'ivanhoe'),
-            get_permalink($parent_post),
+            get_permalink($this->parent_post),
             $game->post_title
         );
         if ($this->move_source) {
